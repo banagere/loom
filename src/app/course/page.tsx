942b4b07f1bca89915d26f9b6874fe6dc6e45245
Ravi Banagere <79131947+banagere@users.noreply.github.com>
2024-12-25 10:44:24 +0530

    simplified code for `/course`

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -2,10 +2,41 @@ import Link from "next/link";
 
 export default function Course() {
   const link = "duration-500 hover:opacity-50";
-  // const link =
-  //   "flex flex-col duration-500 h-24 justify-around rounded-xl border-neutral-300 border shadow-md hover:shadow-lg hover:border-neutral-400 p-5";
   const title = "text-lg font-medium flex";
   const subtitle = "text-neutral-600";
+
+  const list = [
+    {
+      id: 1,
+      href: "https://www.markmeldrum.com/courses/portfolio-construction-management/",
+      title: "Portfolio Construction & Management",
+      subtitle: "Mark Meldrum",
+    },
+    {
+      id: 2,
+      href: "https://www.youtube.com/playlist?list=PLLLuxp_C798FgdhHllQ8pvzUv-_D1PhF0",
+      title: "Learn Finance C++",
+      subtitle: "Andy Duncan",
+    },
+    {
+      id: 3,
+      href: "https://www.youtube.com/playlist?list=PLA2Lu2TlMqzVPeD043744URgdejZf1vlr",
+      title: "Quantitative Trading Strategies",
+      subtitle: "Liu Peng",
+    },
+    {
+      id: 4,
+      href: "https://www.youtube.com/watch?v=7S_tz1z_5bA",
+      title: "SQL Course for Beginners",
+      subtitle: "Programming with Mosh",
+    },
+    {
+      id: 5,
+      href: "https://www.youtube.com/playlist?list=PLhQjrBD2T380Xnv_v683p6UjiKJZe13ki",
+      title: "CS50",
+      subtitle: "Harvard",
+    },
+  ];
 
   return (
     <>
@@ -20,65 +51,18 @@ export default function Course() {
         </div>
 
         <div className="max-w-2xl mx-auto py-5 pt-10">
-          {/* <h1 className="font-medium text-2xl py-3">Channels</h1> */}
-
           <div className="flex flex-col gap-5">
-            <Link
-              href={
-                "https://www.markmeldrum.com/courses/portfolio-construction-management/"
-              }
-              className={link}
-              target="_blank"
-            >
-              <h1 className={title}>
-                Mark Meldrum — Portfolio Construction & Management
-              </h1>
-              <h2 className={subtitle}>
-                Learn to build a portfolio from scratch
-              </h2>
-            </Link>
-
-            <Link
-              href={
-                "https://www.youtube.com/playlist?list=PLLLuxp_C798FgdhHllQ8pvzUv-_D1PhF0"
-              }
-              className={link}
-              target="_blank"
-            >
-              <h1 className={title}>Andy Duncan — Learn Finance C++</h1>
-              <h2 className={subtitle}>Basic C++ in finance application</h2>
-            </Link>
-
-            <Link
-              href={
-                "https://www.youtube.com/playlist?list=PLA2Lu2TlMqzVPeD043744URgdejZf1vlr"
-              }
-              className={link}
-              target="_blank"
-            >
-              <h1 className={title}>Quantitative trading strategies</h1>
-              <h2 className={subtitle}>Liu Peng</h2>
-            </Link>
-
-            <Link
-              href={"https://www.youtube.com/watch?v=7S_tz1z_5bA"}
-              className={link}
-              target="_blank"
-            >
-              <h1 className={title}>SQL Course for Beginners</h1>
-              <h2 className={subtitle}>Programming with Mosh</h2>
-            </Link>
-
-            <Link
-              href={
-                "https://www.youtube.com/playlist?list=PLhQjrBD2T380Xnv_v683p6UjiKJZe13ki"
-              }
-              className={link}
-              target="_blank"
-            >
-              <h1 className={title}>Harvard — CS50</h1>
-              <h2 className={subtitle}>Learn to code from scratch</h2>
-            </Link>
+            {list.map((list) => (
+              <Link
+                href={list.href}
+                className={link}
+                target="_blank"
+                key={list.id}
+              >
+                <h1 className={title}>{list.title}</h1>
+                <h2 className={subtitle}>{list.subtitle}</h2>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
